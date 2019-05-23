f9f3f44f34da80106c12e49773e388386ad8667a
Marcelo Escobar <escobar@br.systems>
2019-05-23 16:47:28 -0300

    ingredientes seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding Ingredients'
+
+request_uri = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+request_query = ''
+url = "#{request_uri}#{request_query}"
+buffer = open(url).read
+
+result = JSON.parse(buffer)
+
+result['drinks'].each do |ing|
+    ingredient = Ingredient.new
+    
+    ingredient.name = ing.first[1]
+    ingredient.save!
+end
+  
+puts `Seeded #{Ingredient.count()} ingredientes`
